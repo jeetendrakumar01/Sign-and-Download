362a88ad7fecb8e78c280426e275fdceab645627
jeetendrakumar01 <jeetendrakumar5861@gmail.com>
2025-04-10 10:19:28 +0530

    commit

--- Expanse tracker/expanse.js
+++ Expanse tracker/expanse.js
@@ -1,5 +1,5 @@
 
-// element select karo
+
 const itemInput = document.getElementById('item');
 const amountInput = document.getElementById('amount');
 const addBtn = document.getElementById('addBtn');
@@ -8,7 +8,7 @@ const expenseList = document.getElementById('expenseList');
 let editMode = false;
 let itemBeingEdited = null;
 
-// Button pe click hone par
+
 addBtn.addEventListener('click', function () {
   const item = itemInput.value;
   const amount = amountInput.value;
@@ -19,14 +19,14 @@ addBtn.addEventListener('click', function () {
   }
 
   if (editMode) {
-    // Edit mode: update old item
+   
     itemBeingEdited.innerText = `${item} - ₹${amount}`;
     addEditDeleteButtons(itemBeingEdited);
-    addBtn.innerText = "Add"; // Button text wapas "Add"
+    addBtn.innerText = "Add"; 
     editMode = false;
     itemBeingEdited = null;
   } else {
-    // Normal Add mode
+   
     const li = document.createElement('li');
     li.innerText = `${item} - ₹${amount}`;
     addEditDeleteButtons(li);
@@ -37,7 +37,6 @@ addBtn.addEventListener('click', function () {
   amountInput.value = '';
 });
 
-// Button add karne ka function
 function addEditDeleteButtons(li) {
   const editBtn = document.createElement('button');
   editBtn.innerText = "✏️";
@@ -47,19 +46,19 @@ function addEditDeleteButtons(li) {
   deleteBtn.innerText = "🗑️";
   deleteBtn.style.marginLeft = "5px";
 
-  // Delete Logic
+  
   deleteBtn.addEventListener('click', function () {
     li.remove();
   });
 
-  // Edit Logic
+  
   editBtn.addEventListener('click', function () {
     const [itemText, amountText] = li.innerText.split(" - ₹");
     itemInput.value = itemText;
     amountInput.value = amountText;
     editMode = true;
     itemBeingEdited = li;
-    addBtn.innerText = "Update Data"; // Button ka naam badal do
+    addBtn.innerText = "Update Data"; 
   });
 
   li.appendChild(editBtn);
